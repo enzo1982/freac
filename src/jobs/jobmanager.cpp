023dfc046998da26504b0c97cb6fff791052b3af
Robert Kausch <robert.kausch@freac.org>
2013-08-24 18:01:22 +0000

    Fixed job error summary dialog showing up in freaccmd.

--- src/jobs/jobmanager.cpp
+++ src/jobs/jobmanager.cpp
@@ -1,5 +1,5 @@
  /* BonkEnc Audio Encoder
-  * Copyright (C) 2001-2011 Robert Kausch <robert.kausch@bonkenc.org>
+  * Copyright (C) 2001-2013 Robert Kausch <robert.kausch@bonkenc.org>
   *
   * This program is free software; you can redistribute it and/or
   * modify it under the terms of the "GNU General Public License".
@@ -78,7 +78,7 @@ Void BonkEnc::JobManager::OnFinishJob(Job *job)
 
 	job->onFinishJob.Disconnect(&JobManager::OnFinishJob, this);
 
-	if (job->GetErrors().Length() > 0)
+	if (job->GetErrors().Length() > 0 && !config->enable_console)
 	{
 		if (config->GetIntValue(Config::CategorySettingsID, Config::SettingsDisplayErrorsID, Config::SettingsDisplayErrorsDefault))
 		{
