262e9da7808baf6247264f9da2ca57e0b7a8a816
Robert Kausch <robert.kausch@freac.org>
2014-01-17 16:20:51 +0000

    Added support for ripping hidden track one audio (HTOA).

--- src/jobs/joblist/addtracks.cpp
+++ src/jobs/joblist/addtracks.cpp
@@ -1,5 +1,5 @@
  /* fre:ac - free audio converter
-  * Copyright (C) 2001-2013 Robert Kausch <robert.kausch@freac.org>
+  * Copyright (C) 2001-2014 Robert Kausch <robert.kausch@freac.org>
   *
   * This program is free software; you can redistribute it and/or
   * modify it under the terms of the "GNU General Public License".
@@ -113,6 +113,18 @@ Error BonkEnc::JobAddTracks::Perform()
 			}
 
 			if (foundTrack) continue;
+		}
+
+		/* Set special title for hidden tracks.
+		 */
+		if (track.isCDTrack && track.cdTrack == 0)
+		{
+			BoCA::I18n	*i18n = BoCA::I18n::Get();
+			Info		 info = track.GetInfo();
+
+			info.title = i18n->TranslateString("hidden track");
+
+			track.SetInfo(info);
 		}
 
 		/* Query CDDB and update track info.
