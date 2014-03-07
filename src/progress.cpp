940efca20b3cfdfbe07b5d34b979257589fc67bf
Robert Kausch <robert.kausch@freac.org>
2014-03-07 19:46:07 +0000

    Fixed a typo.

--- src/progress.cpp
+++ src/progress.cpp
@@ -1,5 +1,5 @@
  /* fre:ac - free audio converter
-  * Copyright (C) 2001-2013 Robert Kausch <robert.kausch@freac.org>
+  * Copyright (C) 2001-2014 Robert Kausch <robert.kausch@freac.org>
   *
   * This program is free software; you can redistribute it and/or
   * modify it under the terms of the "GNU General Public License".
@@ -47,7 +47,7 @@ BonkEnc::Progress::Progress()
 BonkEnc::Progress::~Progress()
 {
 #ifdef __WIN32__
-	/* Relase taakbar interface.
+	/* Relase taskbar interface.
 	 */
 	if (taskbar != NIL)
 	{
