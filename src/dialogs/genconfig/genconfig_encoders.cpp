198e3893df82555c315437221f8a6f238e5494af
Robert Kausch <robert.kausch@freac.org>
2015-08-18 11:51:53 +0000

    Updated Vorbis encoder version string.

--- src/dialogs/genconfig/genconfig_encoders.cpp
+++ src/dialogs/genconfig/genconfig_encoders.cpp
@@ -1,5 +1,5 @@
  /* fre:ac - free audio converter
-  * Copyright (C) 2001-2014 Robert Kausch <robert.kausch@freac.org>
+  * Copyright (C) 2001-2015 Robert Kausch <robert.kausch@freac.org>
   *
   * This program is free software; you can redistribute it and/or
   * modify it under the terms of the "GNU General Public License".
@@ -93,7 +93,7 @@ BonkEnc::GeneralSettingsLayerEncoders::GeneralSettingsLayerEncoders() : Layer(Bo
 
 	if (currentConfig->enable_flac)		combo_encoder->AddEntry(String("FLAC Audio Encoder v").Append(*ex_FLAC__VERSION_STRING));
 	if (currentConfig->enable_lame)		combo_encoder->AddEntry(String("LAME MP3 Encoder v").Append(ex_get_lame_short_version()));
-	if (currentConfig->enable_vorbis)	combo_encoder->AddEntry(String("Ogg Vorbis Encoder v1.3.2"));
+	if (currentConfig->enable_vorbis)	combo_encoder->AddEntry(String("Ogg Vorbis Encoder v1.3.5"));
 	if (currentConfig->enable_wma)		combo_encoder->AddEntry(String("Windows Media Audio Encoder"));
 
 	if (currentConfig->enable_tvq)
