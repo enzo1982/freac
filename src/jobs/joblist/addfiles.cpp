8bfd4c06a419706ff58ddf45775200086f18f8db
Robert Kausch <robert.kausch@freac.org>
2015-09-22 21:13:58 +0000

    Handle empty track returned from decoder components.

--- src/jobs/joblist/addfiles.cpp
+++ src/jobs/joblist/addfiles.cpp
@@ -1,5 +1,5 @@
  /* fre:ac - free audio converter
-  * Copyright (C) 2001-2014 Robert Kausch <robert.kausch@freac.org>
+  * Copyright (C) 2001-2015 Robert Kausch <robert.kausch@freac.org>
   *
   * This program is free software; you can redistribute it and/or
   * modify it under the terms of the "GNU General Public License".
@@ -98,6 +98,10 @@ Error BonkEnc::JobAddFiles::Perform()
 
 			continue;
 		}
+
+		/* Continue if no data present.
+		 */
+		if (track == NIL) continue;
 
 		/* Add disc ID to CD tracks
 		 */
