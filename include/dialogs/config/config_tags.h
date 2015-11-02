0f110ee9d1d7c717020dbdcd4c5fa0f4af20e545
Robert Kausch <robert.kausch@freac.org>
2015-11-02 18:05:45 +0000

    Updated source file headers to clearly state the license version.

--- include/dialogs/config/config_tags.h
+++ include/dialogs/config/config_tags.h
@@ -1,8 +1,10 @@
  /* fre:ac - free audio converter
-  * Copyright (C) 2001-2013 Robert Kausch <robert.kausch@freac.org>
+  * Copyright (C) 2001-2015 Robert Kausch <robert.kausch@freac.org>
   *
   * This program is free software; you can redistribute it and/or
-  * modify it under the terms of the "GNU General Public License".
+  * modify it under the terms of the GNU General Public License as
+  * published by the Free Software Foundation, either version 2 of
+  * the License, or (at your option) any later version.
   *
   * THIS PACKAGE IS PROVIDED "AS IS" AND WITHOUT ANY EXPRESS OR
   * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
--- include/jobs/engine/convert.h
+++ include/jobs/engine/convert.h
@@ -2,7 +2,9 @@
   * Copyright (C) 2001-2015 Robert Kausch <robert.kausch@freac.org>
   *
   * This program is free software; you can redistribute it and/or
-  * modify it under the terms of the "GNU General Public License".
+  * modify it under the terms of the GNU General Public License as
+  * published by the Free Software Foundation, either version 2 of
+  * the License, or (at your option) any later version.
   *
   * THIS PACKAGE IS PROVIDED "AS IS" AND WITHOUT ANY EXPRESS OR
   * IMPLIED WARRANTIES, INCLUDING, WITHOUT LIMITATION, THE IMPLIED
