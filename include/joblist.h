651d0f022ae708de15f1f4f6c37cc9bf6ca4b439
Robert Kausch <robert.kausch@freac.org>
2016-12-22 23:59:37 +0000

    Adapted to smooth class library changes.

--- include/joblist.h
+++ include/joblist.h
@@ -1,5 +1,5 @@
  /* fre:ac - free audio converter
-  * Copyright (C) 2001-2015 Robert Kausch <robert.kausch@freac.org>
+  * Copyright (C) 2001-2016 Robert Kausch <robert.kausch@freac.org>
   *
   * This program is free software; you can redistribute it and/or
   * modify it under the terms of the GNU General Public License as
@@ -33,7 +33,7 @@ namespace BonkEnc
 
 			Array<BoCA::Track>		 tracks;
 
-			Int				 headerTabsHash;
+			UnsignedInt32			 headerTabsHash;
 
 			Bool				 AddTrack(const BoCA::Track &);
 			Bool				 RemoveTrack(const BoCA::Track &);
