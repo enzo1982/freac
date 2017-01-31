4bb28d0dec3791ec76b9202bc48ec41bfc50fa66
Robert Kausch <robert.kausch@freac.org>
2017-01-31 21:45:52 +0000

    Updated Copyright for 2017.

--- src/freac.cpp
+++ src/freac.cpp
@@ -1,5 +1,5 @@
  /* fre:ac - free audio converter
-  * Copyright (C) 2001-2016 Robert Kausch <robert.kausch@freac.org>
+  * Copyright (C) 2001-2017 Robert Kausch <robert.kausch@freac.org>
   *
   * This program is free software; you can redistribute it and/or
   * modify it under the terms of the "GNU General Public License".
@@ -44,7 +44,7 @@ String	 freac::freac::version	    = "v1.0.28";
 String	 freac::freac::shortVersion = "v1.0.28";
 String	 freac::freac::cddbVersion  = "v1.0.27";	// CDDB version may not contain spaces
 String	 freac::freac::cddbMode	    = "submit";
-String	 freac::freac::copyright    = "Copyright (C) 2001-2016 Robert Kausch";
+String	 freac::freac::copyright    = "Copyright (C) 2001-2017 Robert Kausch";
 String	 freac::freac::website	    = "https://www.freac.org/";
 String	 freac::freac::updatePath   = "http://www.freac.org/eUpdate/eUpdate.xml";
 
