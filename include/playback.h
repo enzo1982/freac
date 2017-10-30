a5bf254988a3f0de7803df4a81b41fe2e9fc916d
Robert Kausch <robert.kausch@freac.org>
2017-10-30 23:30:32 +0100

    Use DSP filters during playback.

--- include/playback.h
+++ include/playback.h
@@ -1,5 +1,5 @@
  /* fre:ac - free audio converter
-  * Copyright (C) 2001-2016 Robert Kausch <robert.kausch@freac.org>
+  * Copyright (C) 2001-2017 Robert Kausch <robert.kausch@freac.org>
   *
   * This program is free software; you can redistribute it and/or
   * modify it under the terms of the GNU General Public License as
@@ -20,6 +20,9 @@ using namespace smooth;
 
 namespace freac
 {
+	class Decoder;
+	class Processor;
+
 	class Playback
 	{
 		private:
@@ -42,6 +45,7 @@ namespace freac
 			Int					 newPosition;
 
 			Int					 PlayThread();
+			Void					 Loop(Decoder *, Processor *);
 		public:
 			Void					 Play(const BoCA::Track &);
 
