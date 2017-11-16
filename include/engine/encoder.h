72cdac13cb96af2a1165b40f4b0926b46aacbffc
Robert Kausch <robert.kausch@freac.org>
2017-11-16 22:48:14 +0100

    Add GetEncodedSamples function to Encoder.

--- include/engine/encoder.h
+++ include/engine/encoder.h
@@ -36,6 +36,9 @@ namespace freac
 			Int64					 bytes;
 			Int64					 offset;
 
+			BoCA::Format				 format;
+			Int64					 encodedSamples;
+
 			Bool					 calculateMD5;
 			Hash::MD5				 md5;
 		public:
@@ -51,10 +54,11 @@ namespace freac
 
 			Void					 SignalChapterChange();
 		accessors:
-			Void					 SetCalculateMD5(Bool nCalculateMD5)	{ calculateMD5 = nCalculateMD5; }
-
 			Bool					 IsLossless() const;
 
+			Int64					 GetEncodedSamples() const		{ return encodedSamples; }
+
+			Void					 SetCalculateMD5(Bool nCalculateMD5)	{ calculateMD5 = nCalculateMD5; }
 			String					 GetMD5Checksum();
 	};
 };
