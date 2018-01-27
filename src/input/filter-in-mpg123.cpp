95961bfda7a86a78f087aefac324dfe204ef72f3
Robert Kausch <robert.kausch@freac.org>
2018-01-27 17:09:37 +0100

    Fixed handling of invalid Xing/LAME tags.

--- src/input/filter-in-mpg123.cpp
+++ src/input/filter-in-mpg123.cpp
@@ -1,5 +1,5 @@
  /* fre:ac - free audio converter
-  * Copyright (C) 2001-2017 Robert Kausch <robert.kausch@freac.org>
+  * Copyright (C) 2001-2018 Robert Kausch <robert.kausch@freac.org>
   *
   * This program is free software; you can redistribute it and/or
   * modify it under the terms of the "GNU General Public License".
@@ -151,7 +151,7 @@ Bool freac::FilterInMPG123::ReadXingAndLAMETag(InStream *in)
 	 */
 	XHEADDATA	 data;
 
-	data.toc = NIL;
+	memset(&data, 0, sizeof(XHEADDATA));
 
 	if (GetXingHeader(&data, buffer))
 	{
