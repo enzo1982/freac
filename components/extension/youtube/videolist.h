deb934137f0af7ed6a7090eacf55dbc0e38e613f
Robert Kausch <robert.kausch@freac.org>
2018-09-30 21:26:56 +0200

    Fix CRC value type.

--- components/extension/youtube/videolist.h
+++ components/extension/youtube/videolist.h
@@ -1,5 +1,5 @@
  /* fre:ac - free audio converter
-  * Copyright (C) 2001-2016 Robert Kausch <robert.kausch@freac.org>
+  * Copyright (C) 2001-2018 Robert Kausch <robert.kausch@freac.org>
   *
   * This program is free software; you can redistribute it and/or
   * modify it under the terms of the GNU General Public License as
@@ -28,7 +28,7 @@ namespace BoCA
 			Scrollbar	*scrollbar;
 			Int		 scrollbarPos;
 
-			Int		 visibleEntriesChecksum;
+			UnsignedInt32	 visibleEntriesChecksum;
 
 			Int		 GetEntriesWidth() const;
 		public:
