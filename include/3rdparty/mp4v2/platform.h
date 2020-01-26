b0480b42c6e9d286e3fd6355c238f1f3bc2bf78c
Robert Kausch <robert.kausch@freac.org>
2020-01-26 16:07:55 +0100

    Update Visual Studio project for Visual Studio 2019.

--- include/3rdparty/mp4v2/platform.h
+++ include/3rdparty/mp4v2/platform.h
@@ -6,25 +6,7 @@
 #include <stddef.h>
 #include <stdio.h>
 #include <stdarg.h>
-
-// Thanks, MSFT, for making C99 a total PITA.  Declare this not to define any stdint stuff; this is useful
-// if you're going to be using mp4v2 on windows with some other library that defines its own stdint.
-// TODO msft has finally re-included stdint in vs2010, so maybe at some point in the future this won't be needed.
-#ifndef MP4V2_NO_STDINT_DEFS
-    #if defined( _WIN32 ) && !defined( __MINGW32__ )
-        typedef char      int8_t;
-        typedef short     int16_t;
-        typedef int       int32_t;
-        typedef long long int64_t;
-
-        typedef unsigned char      uint8_t;
-        typedef unsigned short     uint16_t;
-        typedef unsigned int       uint32_t;
-        typedef unsigned long long uint64_t;
-    #else
-        #include <stdint.h>
-    #endif
-#endif
+#include <stdint.h>
 
 #if defined( _WIN32 ) || defined( __MINGW32__ )
 #   if defined( MP4V2_EXPORTS )
