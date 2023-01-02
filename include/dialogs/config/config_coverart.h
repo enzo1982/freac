2e5d7fa4fbea5dd5c5ff862b3ee4ff83af11a2cd
Robert Kausch <robert.kausch@freac.org>
2023-01-02 17:51:36 +0100

    Arrange cover art options in tags/files groups instead of read/write.

--- include/dialogs/config/config_coverart.h
+++ include/dialogs/config/config_coverart.h
@@ -1,5 +1,5 @@
  /* fre:ac - free audio converter
-  * Copyright (C) 2001-2022 Robert Kausch <robert.kausch@freac.org>
+  * Copyright (C) 2001-2023 Robert Kausch <robert.kausch@freac.org>
   *
   * This program is free software; you can redistribute it and/or
   * modify it under the terms of the GNU General Public License as
@@ -21,8 +21,12 @@ namespace freac
 	class ConfigureCoverArt : public BoCA::ConfigLayer
 	{
 		private:
-			GroupBox	*group_coverart_read;
+			GroupBox	*group_coverart_tags;
 			CheckBox	*check_coverart_read_tags;
+			CheckBox	*check_coverart_write_tags;
+			ListBox		*list_coverart_write_tags_format;
+
+			GroupBox	*group_coverart_files;
 			CheckBox	*check_coverart_read_files;
 			CheckBox	*check_coverart_read_patterns;
 			EditBox		*edit_coverart_read_patterns;
@@ -30,29 +34,24 @@ namespace freac
 			Text		*text_coverart_read_max;
 			Slider		*slider_coverart_read_max;
 			Text		*text_coverart_read_max_value;
-
-			GroupBox	*group_coverart_write;
-			CheckBox	*check_coverart_write_tags;
-			ListBox		*list_coverart_write_tags_format;
 			CheckBox	*check_coverart_write_files;
 			Text		*text_coverart_write_files_name;
 			EditBox		*edit_coverart_write_files_name;
 			CheckBox	*check_coverart_write_files_ref;
 
 			Bool		 enableCoverArtReadFromTags;
-			Bool		 enableCoverArtReadFromFiles;
-
-			Bool		 coverArtUsePatterns;
-
-			Int		 coverArtMaxFileSize;
-
 			Bool		 enableCoverArtWriteToTags;
+
+			Bool		 enableCoverArtReadFromFiles;
 			Bool		 enableCoverArtWriteToFiles;
 
-			Bool		 enableCoverArtWriteToFilesRef;
+			Bool		 coverArtUsePatterns;
+			Int		 coverArtMaxFileSize;
+
+			Bool		 coverArtWriteRef;
 		slots:
+			Void		 ToggleReadCoverArt();
 			Void		 ToggleWriteCoverArt();
-			Void		 ToggleReadCoverArtFiles();
 			Void		 ToggleUsePatterns();
 
 			Void		 ChangeMaxCoverArtSize(Int);
